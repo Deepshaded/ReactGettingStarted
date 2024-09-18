39169b6f845e41629f4d20661a2826c8d10c763a
Deepshri M <deepshrim613@gmail.com>
2024-09-18 08:47:04 +0530

    Convert functional component to class comp

--- src/components/maincontent.js
+++ src/components/maincontent.js
@@ -1,20 +1,31 @@
-import React, {useState} from "react";
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
 
-function MainContent(props) {
-    const [count, setCount] = useState(props.numTopics);
-    
-    return (
-    <maincontent>
-        <h3>{props.pageTitle}</h3>
-        This is the main content
-        <p>There are {count} questions</p>
-        <button onClick={()=>setCount(count+1)}>
-            Increase question count</button>
-    </maincontent>
-    )
+class MainContent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.pageTitle,
+            count: this.props.numTopics
+        }
+    }
+
+    onClick = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    render() {
+
+        return (
+            <maincontent>
+
+                This is the main content
+                <h3>{this.state.title}</h3>
+                <p>This is no of questions {this.state.count}</p>
+                <button onClick={this.onClick}>Increase no of questions</button>
+            </maincontent>
+        )
+    }
 }
-MainContent.propTypes = {
-    pageTitle : PropTypes.string
-}
+
 export default MainContent;
